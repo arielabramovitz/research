6336245ab89deda802e55cbf394ca5cc4426875f
JEMHcorpus <36383462+JEMHcorpus@users.noreply.github.com>
2026-09-09 11:18:15 +0300

    Update index.tsx
    Change color of final instruction in example #5.

--- src/examples-tab/index.tsx
+++ src/examples-tab/index.tsx
@@ -257,7 +257,10 @@ function ExamplesTab({ showExamplesModal, setShowExamplesModal }: ExampleTabProp
             </div>
           ) : (
             <div>
-              <span>{"שימו לב: במהלך הניסוי יוצגו מדי פעם גם שאלות פשוטות לבדיקת עירנות. ענו עליהן בדיוק לפי מה שיהיה מוסבר בהוראות.\nבהצלחה!"}</span>
+              <div>
+              <span className="tw-whitespace-pre-line tw-pt-2 tw-text-[#006D77]">{"שימו לב: במהלך הניסוי יוצגו מדי פעם גם שאלות פשוטות לבדיקת עירנות. ענו עליהן בדיוק לפי מה שיהיה מוסבר בהוראות.\nבהצלחה!"}</span>
+
+            </div>
               <div className="tw-mt-4 tw-w-full tw-flex tw-justify-center">
                 <Form.Check
                   key={i}
